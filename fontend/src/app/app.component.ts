9b77c4e4cb8b81dce82e40eee253658246bb5ec9
leminhdien99 <dien0922667574@gmail.com>
2024-09-20 13:39:45 +0700

    not1

--- fontend/src/app/app.component.ts
+++ fontend/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './admin/header/header.component';
-// import { FooterComponent } from './admin/footer/footer.component';
+import { SlibarComponent } from './admin/slibar/slibar.component';
+import { FooterComponent } from './admin/footer/footer.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SlibarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
