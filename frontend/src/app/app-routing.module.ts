ca4e551cac06050f495afe6fa4ee01153cdbb875
bao148 <baovgpc06904@fpt.edu.vn>
2024-09-23 09:55:18 +0700

    update client chay duoc router link

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+import { HomeComponent } from './client/home/home.component';
+import { ProductComponent } from './client/product/product.component';
+import { ProductDetailsComponent } from './client/product-details/product-details.component';
+import { CheckoutComponent } from './client/checkout/checkout.component';
+import { CartComponent } from './client/cart/cart.component';
+import { ContactUsComponent } from './client/contact-us/contact-us.component';
+import { BlogComponent } from './client/blog/blog.component';
+import { BlogSingleComponent } from './client/blog-single/blog-single.component';
+import { LoginComponent } from './client/login/login.component';
+import { PagenotfoundComponent } from './client/pagenotfound/pagenotfound.component';
+
+export const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'product', component: ProductComponent},
+  {path:'product-details', component: ProductDetailsComponent},
+  {path:'checkout', component: CheckoutComponent},
+  {path:'cart', component: CartComponent},
+  {path:'contactus', component: ContactUsComponent},
+  {path:'blog', component: BlogComponent},
+  {path:'blog-detail', component: BlogSingleComponent},
+  {path:'login', component: LoginComponent},
+  {path:'**', component: PagenotfoundComponent},
+  
 ];
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forChild(routes),
   ],
   exports: [RouterModule]
 })
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,37 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './client/home/home.component';
-import { ProductComponent } from './client/product/product.component';
-import { PagenotfoundComponent } from './client/pagenotfound/pagenotfound.component';
+import {RouterModule} from '@angular/router';
 
 
-const appRouter : Routes = [
-  {
-      path : '',
-      component: HomeComponent
-  },
-  {
-      path : 'product',
-      component: ProductComponent
-  },
-  {
-      // khi một router nào được gọi mà không có trong phần appRouter thì NotFoundComponent được gọi ra
-      path : '**',  
-      component: PagenotfoundComponent
-  }
-];
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRouter),
+    BrowserModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
